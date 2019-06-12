832d550e698b755ee50b5b6201fc3fcb3689d014
Stefan Kaestle <stefan@mad-kow.de>
2019-06-12 18:07:48 +0200

    Step 3: mutable variables

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
 
     // Let's add a variable
-    let x = 5;
+    let mut x = 5;
+
+    // Re-assigning a value does not work, we need to add the "mut" keyword
+    // Check out the compiler error - it's really helpful
+    x = 6;
+
+    // You will see later why the distinction between read/write accesses to variables
+    // is so important to Rust and how it is used for thread safety.
+    // It allows some extra checks the compiler does that you wouldn't be getting in
+    // languages like C++ or Java
+
+    // Now here comes something interesting:
+    // Rust allows allocating another variable with the same name, that can even have a
+    // different type.
+    let x = 42.0;
 
     // .. and print it
     println!("Hello, world {}!", x);
